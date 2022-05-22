f602aedb4e412b7202c2c0718e345f9acd6cecb3
manuortega12 <manuortega12@gmail.com>
2022-05-22 19:17:41 -0300

    agrego funciones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+function sumaCantidad (producto) {
+    producto = producto + 1
+    return producto
+}
+
+function sumaTotal (acumulado, totalParcial) {
+    acumulado = acumulado + totalParcial
+    return acumulado
+}
+
+
 let comprar = prompt('Desea agregar productos al carro (por SI presione S o s)').toLowerCase()
 let producto = 0
 let totalParcial = 0
@@ -5,12 +16,14 @@ let acumulado = 0
 
 
 while (comprar == 's') {
-    producto = producto + 1
+    producto = sumaCantidad(producto)
     totalParcial = Number(prompt('Ingrese el precio del producto seleccionado'))
-    acumulado = acumulado + totalParcial
+    acumulado = sumaTotal(acumulado, totalParcial)
     comprar = prompt('Desea seguir agregando productos al carro (por SI presione S o s)').toLowerCase()
 
 }
 
 console.log('Usted tiene ' + producto + ' productos en el carro');
 console.log('El total de su compra es de ' + acumulado + ' pesos');
+
+
