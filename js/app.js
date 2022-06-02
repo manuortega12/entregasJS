8fc45e47637beeaa6225bdff14495f8720600f7f
manuortega12 <manuortega12@gmail.com>
2022-06-02 13:49:11 -0300

    genero array y objeto

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 // SIMULADOR
 
 let total = 0
+let monedas = [ {tc: "dolar", valor:  200} , {tc: "euro", valor: 210}]
 
-divisa1 = ['dolar', 200]
-divisa2 = ['euro', 210]
 
 function Cantidad(cant) {
     return cant = Number(prompt('Ingrese la cantidad que desea comprar.'))
@@ -11,7 +10,7 @@ function Cantidad(cant) {
 
 function Operacion(divisa) {
     total = cant * divisa + total
-    return total 
+    return total
 }
 
 const carrito = []
@@ -19,31 +18,31 @@ const carrito = []
 function SeguirComprando () {
     opcion = prompt('Ingrese la moneda que desea comprar: Dolar o Euro.').toLowerCase()
     switch (opcion) {
-        case divisa1[0]:
+        case monedas[0].tc:
             cant = Cantidad()
-            Operacion(divisa1[1])
+            Operacion(monedas[0].valor)
             carrito.push('Selecciono ' + cant + ' dolar/dolares')
             break;
-        case divisa2[0]:
+        case monedas[1].tc:
             cant = Cantidad()
-            Operacion(divisa2[1])
+            Operacion(monedas[1].valor)
             carrito.push('Selecciono ' + cant + ' euro/euros')
-            break;        
+            break;
         default:
             console.log('Por favor ingrese una opcion correcta..');
             break;
         }
 
-    if (confirm('Desea seguir comprando?')) {   
+    if (confirm('Desea seguir comprando?')) {
         SeguirComprando()
     } else {
         console.log('Finalizo compra.');
         console.log(String(carrito));
-    }     
+    }
 }
 
 SeguirComprando()
 
 if (total != 0) {
-    console.log('El total a pagar es de ' + total + ' pesos');    
+    console.log('El total a pagar es de ' + total + ' pesos');
 }
